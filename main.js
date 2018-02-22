69b00499b4831018e030814e86b7af66908cd84b
Sarah Herzog <sarahherzog@fife.ac.uk>
2018-02-22 13:03:05 +0000

    Added and invoked buildMaze function.

--- main.js
+++ main.js
@@ -19,11 +19,58 @@ var mainState = {
         
         // start the physics engine
         game.physics.startSystem(Phaser.Physics.ARCADE); 
+        
+        // make maze
+        this.buildMaze();
     },
     
     update: function() {
 
     },
+    
+    // Maze building function - creates maze based on an array.
+    // Later we can improve this to pass in the array and include enemy positions and more!
+    // We could even set this up to use a tilemap we can edit externally.
+    buildMaze: function() {
+        
+        // make maze a group of objects
+        maze = game.add.group();
+        maze.enableBody = true; // add physics to the maze
+        
+        // An array of 0 and 1 -> 1 represents a block, 0 a space.
+        var blockArray = [
+            [1,1,1,1,1,1,1,1,1,1],
+            [1,0,0,0,1,1,0,0,0,1],
+            [1,0,1,0,1,0,0,1,0,1],
+            [1,0,1,0,1,0,1,1,1,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,1,1,1,0,1,1,1,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,0,1,1,1,0,1,1,0,1],
+            [1,0,0,0,0,0,0,0,0,1],
+            [1,1,1,1,1,1,1,1,1,1]
+            ];
+        
+        // Go through our list of block locations and create blocks in the right places.
+        for (var r=0;r<blockArray.length;r++) {
+            
+            for (var c=0; c<blockArray[r].length;c++) {
+                console.log("Column",c);
+                console.log("Row",r);
+                
+                // Only create a block if there is a 1 in the list in this spot.
+                if(blockArray[r][c]==1) {
+                   var block=game.add.sprite(c*50,r*50,'block');
+                    maze.add(block);
+                }
+            }
+            
+        }
+        
+        // Set all objects in the maze to be immovable - they won't get moved by the physics.
+        maze.setAll('body.immovable', true);
+    },
+    
     
 };
 
