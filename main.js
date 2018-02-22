4dfdc48e1e6e0b29bb9dd7cb2cb98fee83ad541f
Sarah Herzog <sarahherzog@fife.ac.uk>
2018-02-22 12:57:03 +0000

    Better commenting ;)

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var mainState = {
 
     preload: function() {
-        //load graphics
+        // load graphics
         game.load.image('player', 'assets/player.png');
         game.load.image('block', 'assets/block.png');
         game.load.image('baddy', 'assets/baddy.png');
         game.load.image('key', 'assets/key.png');
         game.load.image('door', 'assets/door.png');
+        
+        // load audio
         game.load.audio('pickup', 'assets/pickup.wav');
         game.load.audio('win', 'assets/win.wav');
     },
